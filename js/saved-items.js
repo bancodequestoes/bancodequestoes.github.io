deaf512545e47620a98613bf30d121dfb5d209fc
Thiago Nascimento <thiagodnf@gmail.com>
2018-11-25 02:52:56 -0200

    added download all saved items as zipfile

--- js/saved-items.js
+++ js/saved-items.js
@@ -1,5 +1,29 @@
 
+function getTex(files){
+
+    var str = "";
+
+    str += "\\documentclass[a4paper, 12pt, addpoints]{exam} \n";
+
+    str += "\\usepackage{listings} \n";
+    str += "\\pointpoints{ponto}{pontos}\n"
+    str += "\\begin{document} \n";
+
+    str += "    \\begin{questions}\n";
+
+    $.each(files, function(i, file){
+        str += "    \\question[1] \\input{questoes/"+file+"}\n";
+    });
+
+    str += "    \\end{questions}\n";
+    str += "\\end{document}\n";
+
+    return str;
+}
+
 $(function(){
+
+    var zip = new JSZip();
 
    var savedItems = getItem("saved-items") || [];
 
@@ -28,5 +52,36 @@ $(function(){
         saveItem("saved-items", items);
 
         location.reload();
-      });
+    });
+
+    $("#download-as-zip-file").click(function(){
+
+        var files = [];
+        var questionsFolder = zip.folder("questoes");
+
+        $.each(savedItems, function(i, item){
+
+            files.push(item.title);
+
+            var url = "https://api.github.com/repos/bancodequestoes/linguagem-c/contents/";
+            var path = item.path;
+
+            $.ajax({
+                async: false,
+                url: url+path,
+                type: "GET",
+                success: function(file) {
+                    questionsFolder.file(file.name, file.content, {base64: true});
+                }
+            });
+        });
+
+        zip.file("prova.tex", getTex(files));
+
+        zip.generateAsync({type:"blob"}).then(function (blob) { // 1) generate the zip file
+            saveAs(blob, "prova.zip");                          // 2) trigger the download
+        }, function (err) {
+            alert(err);
+        });
+    });
 });
