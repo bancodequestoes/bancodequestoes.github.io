288608273c579a532d9d377d2331bcdbb96150c8
Thiago Nascimento <thiagodnf@gmail.com>
2018-11-25 11:43:51 -0200

    added random question

--- js/script.js
+++ js/script.js
@@ -17,4 +17,36 @@ $(function(){
 
         return true;
     });
+
+    $("#btn-i-am-lucky").click(function(){
+
+        var url = "https://api.github.com/repos/bancodequestoes/linguagem-c/git/trees/master";
+
+        $.ajax({url: url, type: "GET",
+            success: function(files) {
+
+                var folders = files.tree || [];
+
+                folders = folders.filter(i => i.type === "tree");
+                var folder = folders[Math.floor(Math.random() * folders.length)];
+
+                $.ajax({ url: folder.url, type: "GET",
+                    success: function(files) {
+
+                        var items = files.tree || [];
+                        var item = items[Math.floor(Math.random() * items.length)];
+
+                        var url = "https://api.github.com/repos/bancodequestoes/linguagem-c/contents/";
+                        var path = folder.path + "/" + item.path;
+
+                        $.ajax({ url: url+path, type: "GET",
+                            success: function(file) {
+                                window.location = file.html_url;
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    })
 });
