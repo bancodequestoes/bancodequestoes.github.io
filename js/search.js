722951221e6908b251b9307af123c7f6e22f3df6
Thiago Nascimento <thiagodnf@gmail.com>
2018-11-25 01:02:01 -0200

    added file download support

--- js/search.js
+++ js/search.js
@@ -45,7 +45,7 @@ function getHTMLForPages(page, selectedPage, url){
     return html;
 }
 
-function getHTMLForSearchResultItem(title, subtitle, url, content){
+function getHTMLForSearchResultItem(title, subtitle, url, content, path){
 
     var html = "";
 
@@ -53,7 +53,14 @@ function getHTMLForSearchResultItem(title, subtitle, url, content){
     html += "<div class='col-12'>";
     html += "<a href='" + url + "'>";
     html += "<h3>" + title + "</h3>";
-    html += "<span>"+subtitle+"</span>";
+    html += "<div class='dropdown'>";
+    html += "<span>"+subtitle+"</span>&nbsp;<a href='#' class='dropdown-toggle' id='dropdownMenuButton' data-toggle='dropdown'></a>";
+    html += "        <div class='dropdown-menu dropdown-menu-right' >";
+    html += "             <a class='dropdown-item' href='#'>Save</a>";
+    html += "           <div class='dropdown-divider'></div>";
+    html += "            <a class='dropdown-item search-result-download' href='#' data-path='"+path+"'>Download</a>";
+    html += "        </div>";
+    html += "    </div>";
     html += "</a>";
     html += "<p>" + content + "...</p>";
     html += "</div>";
@@ -65,6 +72,22 @@ function getHTMLForSearchResultItem(title, subtitle, url, content){
 function decodeUrlParameter(str) {
 	return decodeURIComponent((str+'').replace(/\+/g, '%20'));
 }
+
+function downloadFileAsBase64(filename, text) {
+
+    var element = document.createElement('a');
+
+    element.setAttribute('href', 'data:application/octet-stream;charset=utf-8;base64,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
 
 $(function(){
 
@@ -107,8 +130,9 @@ $(function(){
                     var subtitle = item.path;
                     var url = item.html_url;
                     var content = item.text_matches[0].fragment;
+                    var path = item.path;
 
-                    $(".search-result").append(getHTMLForSearchResultItem(title, subtitle, url,content))
+                    $(".search-result").append(getHTMLForSearchResultItem(title, subtitle, url,content, path))
                 });
 
                 if( total > maxPage){
@@ -119,6 +143,9 @@ $(function(){
                         $(".pages ul").append(getHTMLForPages(i, selectedPage, url+"&page="+i));
                     }
                 }
+            },
+            error: function(a,b,c){
+                $(".search-errors").html("<p><strong>"+a.status+" "+a.statusText+"</strong></p>"+a.responseJSON.message);
             }
         });
     }
@@ -136,4 +163,18 @@ $(function(){
 
         return true;
     });
+
+    $('body').on('click', '.search-result-download', function() {
+
+       var url = "https://api.github.com/repos/bancodequestoes/linguagem-c/contents/";
+       var path = $(this).data("path");
+
+        $.ajax({
+            url: url+path,
+            type: "GET",
+            success: function(file) {
+                downloadFileAsBase64(file.name, file.content);
+            }
+        });
+     });
 });
